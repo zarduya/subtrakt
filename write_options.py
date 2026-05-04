615dc944f09b9a713718c32de03ffb2019bda4b1
m4yruss <vivayrus999@gmail.com>
2026-05-04 18:10:35 +0100

    add secret to authOptions

--- write_options.py
+++ write_options.py
@@ -1,6 +1,7 @@
 code = """import GoogleProvider from "next-auth/providers/google"
 
 export const authOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
